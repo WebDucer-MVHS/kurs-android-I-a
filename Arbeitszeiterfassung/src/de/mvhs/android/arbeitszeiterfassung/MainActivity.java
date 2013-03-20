0ca4b6bf15a1864c2c798e0aebb3463142f91f0d
WebDucer <android@webducr.de>
2013-03-20 19:05:21 +0100

    Beispiel-Implementierung der Hausaufgaben zum Tag 1

--- Arbeitszeiterfassung/src/de/mvhs/android/arbeitszeiterfassung/MainActivity.java
+++ Arbeitszeiterfassung/src/de/mvhs/android/arbeitszeiterfassung/MainActivity.java
@@ -2,34 +2,50 @@ package de.mvhs.android.arbeitszeiterfassung;
 
 import java.util.Date;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
+  // Klassen-Variablen
+  private boolean _IsOpen = false;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        // UI Elemente binden
-        Button cmdStartEnd = (Button)findViewById(R.id.button1);
-        final EditText txtStartTime = (EditText)findViewById(R.id.txt_start_time);
-        
-        cmdStartEnd.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				Date dtmNow = new Date();
-				
-				txtStartTime.setText(dtmNow.toString());
-			}
-		});
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    // UI Elemente binden
+    final Button cmdStartEnd = (Button) findViewById(R.id.cmd_start_stop);
+    final EditText txtStartTime = (EditText) findViewById(R.id.txt_start_time);
+    final EditText txtEndTime = (EditText) findViewById(R.id.txt_end_time);
+
+    if (_IsOpen) {
+      cmdStartEnd.setText(R.string.cmd_end);
+    } else {
+      cmdStartEnd.setText(R.string.cmd_start);
     }
-    
+
+    cmdStartEnd.setOnClickListener(new OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        Date dtmNow = new Date();
+
+        if (_IsOpen) {
+          txtEndTime.setText(dtmNow.toString());
+          _IsOpen = false;
+          cmdStartEnd.setText(R.string.cmd_start);
+        } else {
+          txtStartTime.setText(dtmNow.toString());
+          _IsOpen = true;
+          cmdStartEnd.setText(R.string.cmd_end);
+          txtEndTime.setText("");
+        }
+      }
+    });
+  }
 }
