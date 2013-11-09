59cc142d7e69d43fe5e034ffa46b9bb8d0bd9113
WebDucer <android@webducr.de>
2013-11-09 14:28:20 +0100

    Musterlösung für Hausaufgaben vom 04.11.2013

--- Zeiterfassung/src/de/mvhs/android/zeiterfassung/MainActivity.java
+++ Zeiterfassung/src/de/mvhs/android/zeiterfassung/MainActivity.java
@@ -1,100 +1,163 @@
 package de.mvhs.android.zeiterfassung;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.Date;
 
 import android.app.Activity;
+import android.content.ContentUris;
 import android.content.ContentValues;
-import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import de.mvhs.android.zeiterfassung.db.DBHelper;
 import de.mvhs.android.zeiterfassung.db.ZeitProvider;
 
 public class MainActivity extends Activity {
-	private boolean _IsStarted = false;
-	private final static String _IS_STARTED_KEY = "IsStarted";
+  private boolean                 _IsStarted      = false;
+  private final static String     _EMPTY          = "";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+  private final static DateFormat _DateTimeFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
 
-		// Suchen der Elemente in eingebundenen Layout
-		Button starten = (Button) findViewById(R.id.button_start);
-		Button beenden = (Button) findViewById(R.id.button_end);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-		// Alternative
-		starten.setOnClickListener(onClicked);
-		beenden.setOnClickListener(onClicked);
+    // Suchen der Elemente in eingebundenen Layout
+    Button starten = (Button) findViewById(R.id.button_start);
+    Button beenden = (Button) findViewById(R.id.button_end);
 
-		// Aktivieren / Deaktivieren
-		_IsStarted = savedInstanceState == null ? false : savedInstanceState
-				.getBoolean(_IS_STARTED_KEY, false);
-		changeButtonState();
+    // Alternative
+    starten.setOnClickListener(onClicked);
+    beenden.setOnClickListener(onClicked);
 
-		DBHelper helper = new DBHelper(this);
-		helper.getReadableDatabase();
-		helper.close();
-	}
+    DBHelper helper = new DBHelper(this);
+    helper.getReadableDatabase();
+    helper.close();
+  }
 
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		outState.putBoolean(_IS_STARTED_KEY, _IsStarted);
-		super.onSaveInstanceState(outState);
-	}
+  @Override
+  protected void onStart() {
+    super.onStart();
 
-	private void changeButtonState() {
-		// Buttons suchen
-		Button starten = (Button) findViewById(R.id.button_start);
-		Button beenden = (Button) findViewById(R.id.button_end);
+    String where = "IFNULL(" + ZeitProvider.Columns.END + ", '') = ''";
+    Cursor data = getContentResolver().query(ZeitProvider.CONTENT_URI, null, where, null, null);
 
-		// Aktivieren / Deaktivieren
-		if (_IsStarted) {
-			starten.setEnabled(false);
-			beenden.setEnabled(true);
-		} else {
-			beenden.setEnabled(false);
-			starten.setEnabled(true);
-		}
-	}
+    EditText start = (EditText) findViewById(R.id.text_start_time);
+    EditText ende = (EditText) findViewById(R.id.text_end_time);
 
-	// Alternative
-	private OnClickListener onClicked = new OnClickListener() {
+    if (data != null && data.moveToFirst()) {
+      // Ein leerer Eintrag gefunden
+      try {
+        String startZeit = data.getString(data.getColumnIndex(ZeitProvider.Columns.START));
+        Date startTime = ZeitProvider.DB_DATE_FORMAT.parse(startZeit);
+        start.setText(_DateTimeFormat.format(startTime));
+      } catch (ParseException e) {
+        e.printStackTrace();
+        start.setText(_EMPTY);
+        Toast.makeText(this, "Wrong Date Time Format in the Data Base!", Toast.LENGTH_LONG).show();
+      }
+      _IsStarted = true;
+    } else {
+      start.setText(_EMPTY);
+      _IsStarted = false;
+    }
 
-		@Override
-		public void onClick(View v) {
-			// Aktuelle Uhrzeit bestimmen
-			Date jetzt = new Date();
+    ende.setText(_EMPTY);
 
-			// Textbox
-			EditText ausgabe = null;
+    changeButtonState();
+  }
 
-			if (v.getId() == R.id.button_start) {
-				ausgabe = (EditText) findViewById(R.id.text_start_time);
-				_IsStarted = true;
-				// Daten in die Datenbank spiechern
-				ContentValues values = new ContentValues();
-				values.put(ZeitProvider.Columns.START,
-						ZeitProvider.DB_DATE_FORMAT.format(jetzt));
+  private void changeButtonState() {
+    // Buttons suchen
+    Button starten = (Button) findViewById(R.id.button_start);
+    Button beenden = (Button) findViewById(R.id.button_end);
 
-				Uri insert = getContentResolver().insert(
-						ZeitProvider.CONTENT_URI, values);
+    // Aktivieren / Deaktivieren
+    if (_IsStarted) {
+      starten.setEnabled(false);
+      beenden.setEnabled(true);
+    } else {
+      beenden.setEnabled(false);
+      starten.setEnabled(true);
+    }
+  }
 
-			} else if (v.getId() == R.id.button_end) {
-				ausgabe = (EditText) findViewById(R.id.text_end_time);
-				_IsStarted = false;
+  private void insertNewItem() {
+    // Ausgabe Element suchen
+    EditText start = (EditText) findViewById(R.id.text_start_time);
 
-				Intent editIntent = new Intent(MainActivity.this,
-						EditActivity.class);
-				startActivity(editIntent);
-			}
+    // Aktuelles Datum bestimmen
+    Date jetzt = new Date();
+    _IsStarted = true;
+    // Daten in die Datenbank spiechern
+    ContentValues values = new ContentValues();
+    values.put(ZeitProvider.Columns.START, ZeitProvider.DB_DATE_FORMAT.format(jetzt));
 
-			ausgabe.setText(jetzt.toString());
-			changeButtonState();
-		}
-	};
+    Uri insert = getContentResolver().insert(ZeitProvider.CONTENT_URI, values);
+
+    if (insert == null) {
+      Toast.makeText(this, "Insert of new Entry failed", Toast.LENGTH_LONG).show();
+    } else {
+      Toast.makeText(this, "Entry saved!", Toast.LENGTH_LONG).show();
+    }
+
+    start.setText(_DateTimeFormat.format(jetzt));
+  }
+
+  private void updateItem() {
+    // Ausgabe Element suchen
+    EditText ende = (EditText) findViewById(R.id.text_end_time);
+
+    // Aktuelles Datum bestimmen
+    Date jetzt = new Date();
+    _IsStarted = false;
+
+    // Datensatz suchen
+    String where = "IFNULL(" + ZeitProvider.Columns.END + ", '') = ''";
+    Cursor data = getContentResolver().query(ZeitProvider.CONTENT_URI, new String[] { BaseColumns._ID }, where, null, null);
+
+    if (data != null && data.moveToFirst()) {
+      long id = data.getLong(0);
+
+      // Daten in die Datenbank aktualisieren
+      ContentValues values = new ContentValues();
+      values.put(ZeitProvider.Columns.END, ZeitProvider.DB_DATE_FORMAT.format(jetzt));
+      Uri insertUri = ContentUris.withAppendedId(ZeitProvider.CONTENT_URI, id);
+
+      int updated = getContentResolver().update(insertUri, values, null, null);
+
+      if (updated != 1) {
+        Toast.makeText(this, "Update of new Entry failed", Toast.LENGTH_LONG).show();
+      } else {
+        Toast.makeText(this, "Entry updated!", Toast.LENGTH_LONG).show();
+      }
+
+      ende.setText(_DateTimeFormat.format(jetzt));
+    }
+  }
+
+  // Alternative
+  private OnClickListener onClicked = new OnClickListener() {
+
+                                      @Override
+                                      public void onClick(View v) {
+
+                                        if (v.getId() == R.id.button_start) {
+                                          insertNewItem();
+
+                                        } else if (v.getId() == R.id.button_end) {
+                                          updateItem();
+                                        }
+
+                                        changeButtonState();
+                                      }
+                                    };
 }
--- Zeiterfassung/src/de/mvhs/android/zeiterfassung/db/ZeitProvider.java
+++ Zeiterfassung/src/de/mvhs/android/zeiterfassung/db/ZeitProvider.java
@@ -1,6 +1,7 @@
 package de.mvhs.android.zeiterfassung.db;
 
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import android.content.ContentProvider;
 import android.content.ContentResolver;
@@ -12,169 +13,197 @@ import android.net.Uri;
 import android.provider.BaseColumns;
 
 public class ZeitProvider extends ContentProvider {
-	/* Kalassen-Variblen */
-	private DBHelper _DBHelper = null;
-	private final static UriMatcher _URI_MATCHER = new UriMatcher(
-			UriMatcher.NO_MATCH);
+  /* Kalassen-Variblen */
+  private DBHelper                     _DBHelper          = null;
+  private final static UriMatcher      _URI_MATCHER       = new UriMatcher(UriMatcher.NO_MATCH);
 
-	/* Public */
-	private final static String _DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm";
-	/**
-	 * Formatierer für das richtige Datumsformat in der Datenbank
-	 */
-	public final static SimpleDateFormat DB_DATE_FORMAT = new SimpleDateFormat(
-			_DATE_TIME_PATTERN);
-	/**
-	 * Basispfad für den Content Provider
-	 */
-	public final static String BASE_PATH = "de.mvhs.android.zeiterfassung.zeitprovider";
+  /* Public */
+  private final static String          _DATE_TIME_PATTERN = "yyyy-MM-dd'T'HH:mm";
+  /**
+   * Formatierer für das richtige Datumsformat in der Datenbank
+   */
+  public final static SimpleDateFormat DB_DATE_FORMAT     = new SimpleDateFormat(_DATE_TIME_PATTERN, Locale.ROOT);
+  /**
+   * Basispfad für den Content Provider
+   */
+  public final static String           BASE_PATH          = "de.mvhs.android.zeiterfassung.zeitprovider";
 
-	/**
-	 * Authority des Content Providers (eindeutige Kennung innerhalb des
-	 * Betriebssystems)
-	 */
-	public final static Uri AUTHORITY_URI = Uri.parse("content://" + BASE_PATH);
+  /**
+   * Authority des Content Providers (eindeutige Kennung innerhalb des Betriebssystems)
+   */
+  public final static Uri              AUTHORITY_URI      = Uri.parse("content://" + BASE_PATH);
 
-	/**
-	 * Pfad zur Tabelle
-	 */
-	public final static String CONTENT_DIRECTORY = "zeit";
+  /**
+   * Pfad zur Tabelle
+   */
+  public final static String           CONTENT_DIRECTORY  = "zeit";
 
-	/**
-	 * Uri für den Zugriff auf die Tabelle "Zeit"
-	 */
-	public final static Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI,
-			CONTENT_DIRECTORY);
+  /**
+   * Uri für den Zugriff auf die Tabelle "Zeit"
+   */
+  public final static Uri              CONTENT_URI        = Uri.withAppendedPath(AUTHORITY_URI, CONTENT_DIRECTORY);
 
-	/**
-	 * Datentyp für die Auflistung der Daten aus der Tabelle "Zeit"
-	 */
-	public final static String CONTENT_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE
-			+ "/" + CONTENT_DIRECTORY;
+  /**
+   * Datentyp für die Auflistung der Daten aus der Tabelle "Zeit"
+   */
+  public final static String           CONTENT_TYPE       = ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_DIRECTORY;
 
-	/**
-	 * Datentyp für einen einzigen Datensatz aus der tabelle "Zeit"
-	 */
-	public final static String CONTENT_ITEM_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE
-			+ "/" + CONTENT_DIRECTORY;
+  /**
+   * Datentyp für einen einzigen Datensatz aus der tabelle "Zeit"
+   */
+  public final static String           CONTENT_ITEM_TYPE  = ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_DIRECTORY;
 
-	/**
-	 * Spalten der Tabelle "Zeit"
-	 */
-	public static interface Columns extends BaseColumns {
-		/**
-		 * Satrtzeit Spalte im Format '2013-10-17T14:37'
-		 */
-		public final static String START = "start";
+  /**
+   * Spalten der Tabelle "Zeit"
+   */
+  public static interface Columns extends BaseColumns {
+    /**
+     * Satrtzeit Spalte im Format '2013-10-17T14:37'
+     */
+    public final static String START = "start";
 
-		/**
-		 * Endzeit Splate im Format '2013-10-17T18:55'
-		 */
-		public final static String END = "end";
-	}
+    /**
+     * Endzeit Splate im Format '2013-10-17T18:55'
+     */
+    public final static String END   = "end";
+  }
 
-	static {
-		_URI_MATCHER.addURI(BASE_PATH, CONTENT_DIRECTORY, ZeitenTable.ITEMS);
-		_URI_MATCHER.addURI(BASE_PATH, CONTENT_DIRECTORY + "/#",
-				ZeitenTable.ITEM_ID);
-	}
+  static {
+    _URI_MATCHER.addURI(BASE_PATH, CONTENT_DIRECTORY, ZeitenTable.ITEMS);
+    _URI_MATCHER.addURI(BASE_PATH, CONTENT_DIRECTORY + "/#", ZeitenTable.ITEM_ID);
+  }
 
-	@Override
-	public int delete(Uri uri, String where, String[] whereParams) {
-		int deletedItems = 0;
+  @Override
+  public boolean onCreate() {
+    _DBHelper = new DBHelper(getContext());
+    return _DBHelper != null;
+  }
 
-		final int uriType = _URI_MATCHER.match(uri);
+  @Override
+  public String getType(Uri uri) {
+    String returnType = null;
 
-		switch (uriType) {
-		case ZeitenTable.ITEMS:
-			deletedItems = _DBHelper.getWritableDatabase().delete(
-					ZeitenTable.TABLE_NAME, where, whereParams);
-			break;
+    final int uriType = _URI_MATCHER.match(uri);
 
-		case ZeitenTable.ITEM_ID:
-			long id = ContentUris.parseId(uri);
-			String idWhere = Columns._ID + "=?";
-			String[] idArgs = { String.valueOf(id) };
-			deletedItems = _DBHelper.getWritableDatabase().delete(
-					ZeitenTable.TABLE_NAME, idWhere, idArgs);
-			break;
+    switch (uriType) {
+      case ZeitenTable.ITEMS:
+        returnType = CONTENT_TYPE;
+        break;
 
-		default:
-			throw new IllegalArgumentException(
-					"Ich kenne diese URI nicht :-( '" + uri + "'");
-		}
+      case ZeitenTable.ITEM_ID:
+        returnType = CONTENT_ITEM_TYPE;
+        break;
+    }
 
-		if (deletedItems > 0) {
-			getContext().getContentResolver().notifyChange(uri, null);
-		}
+    return returnType;
+  }
 
-		return deletedItems;
-	}
+  @Override
+  public Uri insert(Uri uri, ContentValues values) {
+    Uri insertUri = null;
 
-	@Override
-	public String getType(Uri uri) {
-		String returnType = null;
+    final int uriType = _URI_MATCHER.match(uri);
+    long id = -1;
 
-		final int uriType = _URI_MATCHER.match(uri);
+    switch (uriType) {
+      case ZeitenTable.ITEMS:
+      case ZeitenTable.ITEM_ID:
+        id = _DBHelper.getWritableDatabase().insert(ZeitenTable.TABLE_NAME, null, values);
+        break;
 
-		switch (uriType) {
-		case ZeitenTable.ITEMS:
-			returnType = CONTENT_TYPE;
-			break;
+      default:
+        throw new IllegalArgumentException("Ich kenne diese URI nicht :-( '" + uri + "'");
+    }
 
-		case ZeitenTable.ITEM_ID:
-			returnType = CONTENT_ITEM_TYPE;
-			break;
-		}
+    if (id > 0) {
+      insertUri = ContentUris.withAppendedId(CONTENT_URI, id);
+      getContext().getContentResolver().notifyChange(insertUri, null);
+    }
 
-		return returnType;
-	}
+    return insertUri;
+  }
 
-	@Override
-	public Uri insert(Uri uri, ContentValues values) {
-		Uri insertUri = null;
+  @Override
+  public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+    int updatedItems = 0;
 
-		final int uriType = _URI_MATCHER.match(uri);
-		long id = -1;
+    final int uriType = _URI_MATCHER.match(uri);
 
-		switch (uriType) {
-		case ZeitenTable.ITEMS:
-		case ZeitenTable.ITEM_ID:
-			id = _DBHelper.getWritableDatabase().insert(ZeitenTable.TABLE_NAME,
-					null, values);
-			break;
+    switch (uriType) {
+      case ZeitenTable.ITEMS:
+        updatedItems = _DBHelper.getWritableDatabase().update(ZeitenTable.TABLE_NAME, values, selection, selectionArgs);
+        break;
 
-		default:
-			throw new IllegalArgumentException(
-					"Ich kenne diese URI nicht :-( '" + uri + "'");
-		}
+      case ZeitenTable.ITEM_ID:
+        long id = ContentUris.parseId(uri);
+        String idWhere = Columns._ID + "=?";
+        String[] idArgs = { String.valueOf(id) };
+        updatedItems = _DBHelper.getWritableDatabase().update(ZeitenTable.TABLE_NAME, values, idWhere, idArgs);
+        break;
 
-		if (id > 0) {
-			insertUri = ContentUris.withAppendedId(CONTENT_URI, id);
-			getContext().getContentResolver().notifyChange(insertUri, null);
-		}
+      default:
+        throw new IllegalArgumentException("Ich kenne diese URI nicht :-( '" + uri + "'");
+    }
 
-		return insertUri;
-	}
+    if (updatedItems > 0) {
+      getContext().getContentResolver().notifyChange(uri, null);
+    }
 
-	@Override
-	public boolean onCreate() {
-		_DBHelper = new DBHelper(getContext());
-		return _DBHelper != null;
-	}
+    return updatedItems;
+  }
 
-	@Override
-	public Cursor query(Uri uri, String[] projection, String selection,
-			String[] selectionArgs, String sortOrder) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+  @Override
+  public int delete(Uri uri, String where, String[] whereParams) {
+    int deletedItems = 0;
 
-	@Override
-	public int update(Uri uri, ContentValues values, String selection,
-			String[] selectionArgs) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    final int uriType = _URI_MATCHER.match(uri);
+
+    switch (uriType) {
+      case ZeitenTable.ITEMS:
+        deletedItems = _DBHelper.getWritableDatabase().delete(ZeitenTable.TABLE_NAME, where, whereParams);
+        break;
+
+      case ZeitenTable.ITEM_ID:
+        long id = ContentUris.parseId(uri);
+        String idWhere = Columns._ID + "=?";
+        String[] idArgs = { String.valueOf(id) };
+        deletedItems = _DBHelper.getWritableDatabase().delete(ZeitenTable.TABLE_NAME, idWhere, idArgs);
+        break;
+
+      default:
+        throw new IllegalArgumentException("Ich kenne diese URI nicht :-( '" + uri + "'");
+    }
+
+    if (deletedItems > 0) {
+      getContext().getContentResolver().notifyChange(uri, null);
+    }
+
+    return deletedItems;
+  }
+
+  @Override
+  public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+    Cursor returnData = null;
+
+    final int uriType = _URI_MATCHER.match(uri);
+
+    switch (uriType) {
+      case ZeitenTable.ITEMS:
+        returnData = _DBHelper.getReadableDatabase().query(ZeitenTable.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+        break;
+
+      case ZeitenTable.ITEM_ID:
+        long id = ContentUris.parseId(uri);
+        String idWhere = Columns._ID + "=?";
+        String[] idArgs = { String.valueOf(id) };
+        returnData = _DBHelper.getReadableDatabase().query(ZeitenTable.TABLE_NAME, projection, idWhere, idArgs, null, null, null);
+        break;
+
+      default:
+        throw new IllegalArgumentException("Ich kenne diese URI nicht :-( '" + uri + "'");
+    }
+
+    return returnData;
+  }
 
 }
