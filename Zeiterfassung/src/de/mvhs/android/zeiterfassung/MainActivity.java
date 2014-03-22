075b8e8afe9072e36e427ed867878829032288b5
WebDucer <android@webducr.de>
2014-03-22 18:59:56 +0100

    Hausaufgabe zum Tag 1

--- Zeiterfassung/src/de/mvhs/android/zeiterfassung/MainActivity.java
+++ Zeiterfassung/src/de/mvhs/android/zeiterfassung/MainActivity.java
@@ -8,54 +8,80 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		setContentView(R.layout.activity_main);
-	}
-	
-	@Override
-	protected void onStart() {
-		super.onStart();
-		
-		// Buttons registrieren
-		Button commandStart = (Button) findViewById(R.id.StartCommand);
-		Button commandEnd = (Button) findViewById(R.id.EndCommand);
-		
-		// Click Event registrieren
-		commandStart.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// Verhalten beim Klick auf den Strat Button
-				EditText startTime = (EditText) findViewById(R.id.StartTime);
-				startTime.setText(new Date().toString());
-			}
-		});
-		
-		commandEnd.setOnClickListener(new OnClickListener() {
-			
-			@Override
-			public void onClick(View v) {
-				// Verhalten beim Click auf den Ende-Button
-				EditText endTime = (EditText) findViewById(R.id.EndTime);
-				endTime.setText(new Date().toString());
-			}
-		});
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+  private boolean _IsStarted = false;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_main);
+
+    if (savedInstanceState != null) {
+      _IsStarted = savedInstanceState.getBoolean("CurrentState", false);
+    }
+
+    setButtonState();
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+
+    // Buttons registrieren
+    Button commandStart = (Button) findViewById(R.id.StartCommand);
+    Button commandEnd = (Button) findViewById(R.id.EndCommand);
+
+    // Click Event registrieren
+    commandStart.setOnClickListener(new OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        // Verhalten beim Klick auf den Strat Button
+        EditText startTime = (EditText) findViewById(R.id.StartTime);
+        startTime.setText(new Date().toString());
+        _IsStarted = true;
+        setButtonState();
+      }
+    });
+
+    commandEnd.setOnClickListener(new OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        // Verhalten beim Click auf den Ende-Button
+        EditText endTime = (EditText) findViewById(R.id.EndTime);
+        endTime.setText(new Date().toString());
+        _IsStarted = false;
+        setButtonState();
+      }
+    });
+  }
+
+  @Override
+  protected void onSaveInstanceState(Bundle outState) {
+    outState.putBoolean("CurrentState", _IsStarted);
+    super.onSaveInstanceState(outState);
+  }
+
+  @Override
+  protected void onStop() {
+    // Buttons registrieren
+    Button commandStart = (Button) findViewById(R.id.StartCommand);
+    Button commandEnd = (Button) findViewById(R.id.EndCommand);
+
+    // Click Event deregistrieren
+    commandStart.setOnClickListener(null);
+    commandEnd.setOnClickListener(null);
+    super.onStop();
+  }
+
+  private void setButtonState() {
+    Button commandStart = (Button) findViewById(R.id.StartCommand);
+    Button commandEnd = (Button) findViewById(R.id.EndCommand);
+
+    commandStart.setEnabled(_IsStarted == false);
+    commandEnd.setEnabled(_IsStarted);
+  }
 }
