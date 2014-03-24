891604edb290a3d9b18badbdfd9f3c5f6bda6d23
WebDucer <android@webducr.de>
2014-03-24 21:07:54 +0100

    Stand 24.03.2014

--- Zeiterfassung/src/de/mvhs/android/zeiterfassung/MainActivity.java
+++ Zeiterfassung/src/de/mvhs/android/zeiterfassung/MainActivity.java
@@ -3,85 +3,98 @@ package de.mvhs.android.zeiterfassung;
 import java.util.Date;
 
 import android.app.Activity;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import de.mvhs.android.zeiterfassung.db.DBHelper;
 
 public class MainActivity extends Activity {
-  private boolean _IsStarted = false;
+	private boolean _IsStarted = false;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_main);
 
-    if (savedInstanceState != null) {
-      _IsStarted = savedInstanceState.getBoolean("CurrentState", false);
-    }
+		if (savedInstanceState != null) {
+			_IsStarted = savedInstanceState.getBoolean("CurrentState", false);
+		}
 
-    setButtonState();
-  }
+		setButtonState();
+	}
 
-  @Override
-  protected void onStart() {
-    super.onStart();
+	@Override
+	protected void onStart() {
+		super.onStart();
 
-    // Buttons registrieren
-    Button commandStart = (Button) findViewById(R.id.StartCommand);
-    Button commandEnd = (Button) findViewById(R.id.EndCommand);
+		// Buttons registrieren
+		Button commandStart = (Button) findViewById(R.id.StartCommand);
+		Button commandEnd = (Button) findViewById(R.id.EndCommand);
 
-    // Click Event registrieren
-    commandStart.setOnClickListener(new OnClickListener() {
+		// Click Event registrieren
+		commandStart.setOnClickListener(new OnClickListener() {
 
-      @Override
-      public void onClick(View v) {
-        // Verhalten beim Klick auf den Strat Button
-        EditText startTime = (EditText) findViewById(R.id.StartTime);
-        startTime.setText(new Date().toString());
-        _IsStarted = true;
-        setButtonState();
-      }
-    });
+			@Override
+			public void onClick(View v) {
+				// Verhalten beim Klick auf den Strat Button
+				EditText startTime = (EditText) findViewById(R.id.StartTime);
+				startTime.setText(new Date().toString());
+				_IsStarted = true;
+				setButtonState();
 
-    commandEnd.setOnClickListener(new OnClickListener() {
+				DBHelper helper = new DBHelper(MainActivity.this);
+				SQLiteDatabase db = helper.getWritableDatabase();
 
-      @Override
-      public void onClick(View v) {
-        // Verhalten beim Click auf den Ende-Button
-        EditText endTime = (EditText) findViewById(R.id.EndTime);
-        endTime.setText(new Date().toString());
-        _IsStarted = false;
-        setButtonState();
-      }
-    });
-  }
+				ContentValues values = new ContentValues();
+				values.put("start_time", new Date().toString());
 
-  @Override
-  protected void onSaveInstanceState(Bundle outState) {
-    outState.putBoolean("CurrentState", _IsStarted);
-    super.onSaveInstanceState(outState);
-  }
+				db.insert("zeit", null, values);
 
-  @Override
-  protected void onStop() {
-    // Buttons registrieren
-    Button commandStart = (Button) findViewById(R.id.StartCommand);
-    Button commandEnd = (Button) findViewById(R.id.EndCommand);
+				db.close();
+			}
+		});
 
-    // Click Event deregistrieren
-    commandStart.setOnClickListener(null);
-    commandEnd.setOnClickListener(null);
-    super.onStop();
-  }
+		commandEnd.setOnClickListener(new OnClickListener() {
 
-  private void setButtonState() {
-    Button commandStart = (Button) findViewById(R.id.StartCommand);
-    Button commandEnd = (Button) findViewById(R.id.EndCommand);
+			@Override
+			public void onClick(View v) {
+				// Verhalten beim Click auf den Ende-Button
+				EditText endTime = (EditText) findViewById(R.id.EndTime);
+				endTime.setText(new Date().toString());
+				_IsStarted = false;
+				setButtonState();
+			}
+		});
+	}
 
-    commandStart.setEnabled(_IsStarted == false);
-    commandEnd.setEnabled(_IsStarted);
-  }
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putBoolean("CurrentState", _IsStarted);
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	protected void onStop() {
+		// Buttons registrieren
+		Button commandStart = (Button) findViewById(R.id.StartCommand);
+		Button commandEnd = (Button) findViewById(R.id.EndCommand);
+
+		// Click Event deregistrieren
+		commandStart.setOnClickListener(null);
+		commandEnd.setOnClickListener(null);
+		super.onStop();
+	}
+
+	private void setButtonState() {
+		Button commandStart = (Button) findViewById(R.id.StartCommand);
+		Button commandEnd = (Button) findViewById(R.id.EndCommand);
+
+		commandStart.setEnabled(_IsStarted == false);
+		commandEnd.setEnabled(_IsStarted);
+	}
 }
---
+++ Zeiterfassung/src/de/mvhs/android/zeiterfassung/db/DBHelper.java
@@ -0,0 +1,29 @@
+package de.mvhs.android.zeiterfassung.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+	// Datenbank-Datei-Name
+	private final static String _DB_NAME = "zeit.db";
+
+	// Aktuelle Datenbank-Version (benötigte Version)
+	private final static int _DB_VERSION = 1;
+
+	public DBHelper(Context context) {
+		super(context, _DB_NAME, null, _DB_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		ZeitTable.onCreate(db);
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		ZeitTable.onUpgrade(db, oldVersion, newVersion);
+	}
+
+}
