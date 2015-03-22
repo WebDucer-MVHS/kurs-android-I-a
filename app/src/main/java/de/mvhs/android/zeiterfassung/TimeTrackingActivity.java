9081c5693ce8c4c78094d3f06db107c7bcd78515
Eugen Richter <android@webducer.de>
2015-03-22 09:57:41 +0100

    Hausaufgaben Lösung zum Tag 1

--- app/src/main/java/de/mvhs/android/zeiterfassung/TimeTrackingActivity.java
+++ app/src/main/java/de/mvhs/android/zeiterfassung/TimeTrackingActivity.java
@@ -5,70 +5,84 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import java.util.Date;
 
 
 public class TimeTrackingActivity extends ActionBarActivity {
-    // Klassenvariablen
-    // UI Elemente
-    private EditText _startTime = null;
-    private EditText _endTime = null;
-    private Button _startCommand = null;
-    private Button _endCommand = null;
+   // Klassenvariablen
+   // UI Elemente
+   private EditText _startTime = null;
+   private EditText _endTime = null;
+   private Button _startCommand = null;
+   private Button _endCommand = null;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_time_tracking);
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_time_tracking);
 
-        // Suchen der UI Elemente aus dem Layout
-        _startTime = (EditText) findViewById(R.id.StartTime);
-        _endTime = (EditText) findViewById(R.id.EndTime);
-        _startCommand = (Button) findViewById(R.id.StartCommand);
-        _endCommand = (Button) findViewById(R.id.EndCommand);
-    }
+      // Suchen der UI Elemente aus dem Layout
+      _startTime = (EditText) findViewById(R.id.StartTime);
+      _endTime = (EditText) findViewById(R.id.EndTime);
+      _startCommand = (Button) findViewById(R.id.StartCommand);
+      _endCommand = (Button) findViewById(R.id.EndCommand);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+      // Deaktivieren der Bearbeitung in EditText-Feldern
+      _startTime.setKeyListener(null);
+      _endTime.setKeyListener(null);
+   }
 
-        // Registrieren der Listener (Events)
-        _startCommand.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Logik nach dem Klicken des Buttons
-                _startTime.setText(String.valueOf(new Date()));
-            }
-        });
-    }
+   @Override
+   protected void onStart() {
+      super.onStart();
 
-    @Override
-    protected void onStop() {
-        super.onStop();
+      // Registrieren der Listener (Events)
+      _startCommand.setOnClickListener(new OnStartButtonClicked());
+      _endCommand.setOnClickListener(new OnEndButtonClicked());
 
-        // Deregistrieren der Listender (Events)
-        _startCommand.setOnClickListener(null);
-    }
+      // Ende Button nach dem Start deaktivieren
+      _endCommand.setEnabled(false);
+   }
 
+   @Override
+   protected void onStop() {
+      super.onStop();
 
-
-
-
+      // Deregistrieren der Listender (Events)
+      _startCommand.setOnClickListener(null);
+      _endCommand.setOnClickListener(null);
+   }
 
 
+   // Interne Klassen
+   private class OnStartButtonClicked implements View.OnClickListener {
 
+      @Override
+      public void onClick(View v) {
+         // Deaktivieren des Buttons
+         _startCommand.setEnabled(false);
 
+         // Logik nach dem Klicken des Buttons
+         _startTime.setText(String.valueOf(new Date()));
 
+         // Aktivieren des Ende Buttons
+         _endCommand.setEnabled(true);
+      }
+   }
 
+   private class OnEndButtonClicked implements View.OnClickListener {
 
+      @Override
+      public void onClick(View v) {
+         // Dekativieren des Buttons
+         _endCommand.setEnabled(false);
 
+         // Logik nach dem Klicken des Buttons
+         _endTime.setText(String.valueOf(new Date()));
 
-
-
-
-
-
-
+         // Aktivieren des Start Buttons
+         _startCommand.setEnabled(true);
+      }
+   }
 }
