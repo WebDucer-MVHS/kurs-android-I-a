faf2bcfda214f5af7f7e7deda2f601a957afdd83
Eugen Richter <android@webducer.de>
2016-04-12 18:11:38 +0200

    Hausaufgaben vom 06.06.2016

--- app/src/main/java/de/mvhs/android/zeiterfassung/MainActivity.java
+++ app/src/main/java/de/mvhs/android/zeiterfassung/MainActivity.java
@@ -1,11 +1,10 @@
 package de.mvhs.android.zeiterfassung;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import java.util.Date;
 
@@ -14,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_relative);
 
         // Suchen der Elemente vom Layout
         Button startCommand = (Button) findViewById(R.id.StartCommand);
