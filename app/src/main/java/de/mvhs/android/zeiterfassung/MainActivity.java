d7320f6776a03beb5c2863437324965530989b6a
Eugen Richter <android@webducer.de>
2016-10-18 20:29:10 +0200

    Lösung zu Hausaufgaben vom 12.10.2016

--- app/src/main/java/de/mvhs/android/zeiterfassung/MainActivity.java
+++ app/src/main/java/de/mvhs/android/zeiterfassung/MainActivity.java
@@ -10,38 +10,45 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText _startValue;
-    private Button _startCommand;
+  private EditText _startValue;
+  private Button _startCommand;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    // Linear Layout
+    //setContentView(R.layout.activity_main_linear);
 
-        // Suchen der Elemente im Layout
-        _startValue = (EditText) findViewById(R.id.StartTimeValue);
-        _startCommand = (Button) findViewById(R.id.StartCommand);
-    }
+    // Relative layout
+    //setContentView(R.layout.activity_main_relative);
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    // Grid layout
+    setContentView(R.layout.activity_main_grid);
 
-        // Event registrieren
-        _startCommand.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Was soll der Button machen
-                _startValue.setText(new Date().toString());
-            }
-        });
-    }
+    // Suchen der Elemente im Layout
+    _startValue = (EditText) findViewById(R.id.StartTimeValue);
+    _startCommand = (Button) findViewById(R.id.StartCommand);
+  }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
+  @Override
+  protected void onStart() {
+    super.onStart();
 
-        // Event deregistrieren
-        _startCommand.setOnClickListener(null);
-    }
+    // Event registrieren
+    _startCommand.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        // Was soll der Button machen
+        _startValue.setText(new Date().toString());
+      }
+    });
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+
+    // Event deregistrieren
+    _startCommand.setOnClickListener(null);
+  }
 }
