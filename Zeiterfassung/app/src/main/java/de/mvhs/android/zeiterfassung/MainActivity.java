8a74bc9bf398a1dc8b31d25aa4f0e4767688f9c8
Eugen Richter <android@webducer.de>
2017-04-26 22:18:58 +0200

    Stand 26.04.2017

--- Zeiterfassung/app/src/main/java/de/mvhs/android/zeiterfassung/MainActivity.java
+++ Zeiterfassung/app/src/main/java/de/mvhs/android/zeiterfassung/MainActivity.java
@@ -1,58 +1,126 @@
 package de.mvhs.android.zeiterfassung;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.DateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 import de.mvhs.android.zeiterfassung.db.DbHelper;
 import de.mvhs.android.zeiterfassung.db.TimeContract;
 
+import static android.R.attr.data;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button _startCommand;
+    private Button _endCommand;
+    private EditText _startTime;
+    private EditText _endTime;
+    private Uri _data;
+
+    private DateFormat _formatter = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.SHORT);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_linear);
+
+        _startCommand = (Button) findViewById(R.id.StartCommand);
+        _endCommand = (Button) findViewById(R.id.EndCommand);
+
+        _startTime = (EditText) findViewById(R.id.StartTimeValue);
+        _endTime = (EditText) findViewById(R.id.EndTimeValue);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-//        // Speichern in der Datenbank
-//        DbHelper helper = new DbHelper(this);
-//        SQLiteDatabase db = helper.getWritableDatabase();
-//
-//        // Einfügen der Daten
-//        ContentValues values = new ContentValues();
-//        values.put("start_time", new Date().toString());
-//        values.put("end_time", new Date().toString());
-//
-//        long id = db.insert("time", null, values);
-//
-//        db.close();
-//        helper.close();
-
-        /* Content Provider */
-        // Einfügen der Daten
-        ContentValues values = new ContentValues();
-        values.put(TimeContract.TimeData.Columns.START,
-                TimeContract.Converters.formatForDb(Calendar.getInstance()));
-        values.put(TimeContract.TimeData.Columns.END,
-                TimeContract.Converters.formatForDb(Calendar.getInstance()));
-
-        // Speichern über Content Provider
-        getContentResolver().insert(TimeContract.TimeData.CONTENT_URI, values);
+        // Registrieren der Events
+        _startCommand.setOnClickListener(new OnStartClicked());
+        _endCommand.setOnClickListener(new OnStopClicked());
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // Deregistrieren der Events
+        _startCommand.setOnClickListener(null);
+        _endCommand.setOnClickListener(null);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.MenuItemList:
+                Log.d("MainActivity", "Auflistung!");
+                Intent listIntent = new Intent(this, TimeDataListActivity.class);
+                startActivity(listIntent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    public class OnStartClicked implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            Log.d("MainActivity", "Start gecklickt!");
+
+            Calendar currentTime = Calendar.getInstance();
+            String currentTimeDb = TimeContract.Converters.formatForDb(currentTime);
+            ContentValues values = new ContentValues();
+            values.put(TimeContract.TimeData.Columns.START, currentTimeDb);
+
+            // Datensatz einfügen
+            _data = getContentResolver().insert(TimeContract.TimeData.CONTENT_URI, values);
+            Log.d("MainActivity", _data.toString());
+
+            _startTime.setText(_formatter.format(currentTime.getTime()));
+        }
+    }
+
+    public class OnStopClicked implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Log.d("MainActivity", "Ende geklickt!");
+
+            Calendar currentTime = Calendar.getInstance();
+            String currentTimeDb = TimeContract.Converters.formatForDb(currentTime);
+            ContentValues values = new ContentValues();
+            values.put(TimeContract.TimeData.Columns.END, currentTimeDb);
+
+            // Datensatz aktualisieren
+            if(_data != null){
+                int updated = getContentResolver().update(_data, values, null, null);
+
+                Log.d("MainActivity", String.valueOf(updated));
+            }
+
+            _endTime.setText(_formatter.format(currentTime.getTime()));
+        }
+    }
 
 
 
---
+++ Zeiterfassung/app/src/main/java/de/mvhs/android/zeiterfassung/TimeDataListActivity.java
@@ -0,0 +1,92 @@
+package de.mvhs.android.zeiterfassung;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import de.mvhs.android.zeiterfassung.db.TimeContract;
+
+/**
+ * Created by Kurs on 26.04.2017.
+ */
+
+public class TimeDataListActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+    private ListView _list;
+    private SimpleCursorAdapter _adapter;
+
+    private static final int _LOADER_ID = 100;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_data_list);
+
+        _list = (ListView) findViewById(R.id.TimeDataList);
+        _adapter = new SimpleCursorAdapter(this, // Context
+                android.R.layout.simple_list_item_2, // layout für Zeile
+                null, // Daten
+                new String[]{
+                        TimeContract.TimeData.Columns.START,
+                        TimeContract.TimeData.Columns.END}, // Spalten aus Ergebnis
+                new int[]{android.R.id.text1, android.R.id.text2}, // IDs der Views
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
+        _list.setAdapter(_adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        getSupportLoaderManager()
+                .restartLoader(_LOADER_ID, null, this);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        getSupportLoaderManager().destroyLoader(_LOADER_ID);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        CursorLoader loader = null;
+
+        switch (id){
+            case _LOADER_ID:
+                loader = new CursorLoader(
+                        this, // Context
+                        TimeContract.TimeData.CONTENT_URI, // Uri für Provider
+                        null, // Spalten
+                        null, // Filter
+                        null, // Filter Parameter
+                        null); // Sortierung
+        }
+
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        switch (loader.getId()){
+            case _LOADER_ID:
+                _adapter.swapCursor(data);
+                break;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        switch (loader.getId()){
+            case _LOADER_ID:
+                _adapter.swapCursor(null);
+                break;
+        }
+    }
+}
